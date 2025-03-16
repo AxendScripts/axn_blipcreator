2d2086405bb2cc8494baf515ac27707738c275d2
Axend Scripts <olivaracka@gmail.com>
2025-03-16 22:14:28 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -9,15 +9,13 @@ Citizen.CreateThread(function()
 end)
 
 Citizen.CreateThread(function()
-    for _, pedData in pairs(Config.Peds) do
-        RequestModel(pedData.model)
-        while not HasModelLoaded(pedData.model) do
-            Wait(100)
-        end
-
-        local ped = CreatePed(4, pedData.model, pedData.coords.x, pedData.coords.y, pedData.coords.z - 1.0, pedData.heading, false, true)
-        SetEntityInvincible(ped, true)
-        FreezeEntityPosition(ped, true)
-        SetBlockingOfNonTemporaryEvents(ped, true)
+    for _, blip in pairs(Config.Blips) do
+        local newBlip = AddBlipForCoord(blip.coords)
+        SetBlipSprite(newBlip, blip.sprite)
+        SetBlipColour(newBlip, blip.color)
+        SetBlipScale(newBlip, blip.scale)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentSubstringPlayerName(blip.name)
+        EndTextCommandSetBlipName(newBlip)
     end
 end)
